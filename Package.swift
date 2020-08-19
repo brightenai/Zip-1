7f149db6e372aed17596afbc519b32e703885816
John Burkey <johnburkey@brighten.ai>
2020-08-19 07:26:15 -0500

    D

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -12,18 +12,30 @@ let package = Package(
             name: "Minizip",
             dependencies: [],
             path: "Zip/minizip",
-            exclude: ["module"],
+            exclude: [
+                "module"
+            ],
             linkerSettings: [
-                .linkedLibrary("z")
+                .linkedLibrary("z"),
+                .unsafeFlags([ "-Xlinker","-soname=libZip.so"],.when(platforms: [.android])),
             ]),
         .target(
             name: "Zip",
             dependencies: ["Minizip"],
             path: "Zip",
-            exclude: ["minizip", "zlib"]),
+            exclude: ["minizip", "zlib","Info.plist","Info-tvOS.plist"]),
         .testTarget(
             name: "ZipTests",
             dependencies: ["Zip"],
-            path: "ZipTests"),
+            path: "ZipTests",
+            exclude: [
+            "Resources/unsupported_permissions.zip",
+              "Resources/3crBXeO.gif",
+              "Resources/bb8.zip",
+              "Resources/permissions.zip",
+              "Resources/kYkLkPf.gif",
+              "Info.plist",
+            ]
+            ),
     ]
 )
